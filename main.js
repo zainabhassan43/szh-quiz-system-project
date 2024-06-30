3b05b2baa178c0f55f4986c8b44fc2086a49269d
zainabhassan43 <zainab124hassan@gmail.com>
2024-06-30 11:13:46 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,76 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const quiz = await inquirer.prompt([
+    {
+        name: "question_1",
+        type: "list",
+        message: "Q1.What is the correct way to check if two values are not equal in Typescript?",
+        choices: ["a == b", "a === b", "a = b", "a !== b"]
+    },
+    {
+        name: "question_2",
+        type: "list",
+        message: "Q2.Which of the following characters is allowed in variable names in Typescript?",
+        choices: ["$", "@", "#", "&"]
+    },
+    {
+        name: "question_3",
+        type: "list",
+        message: "Q3.Which operator is commonly used for string concatenation in Typescript?",
+        choices: ["+", "-", "*", "/"]
+    },
+    {
+        name: "question_4",
+        type: "list",
+        message: "Q4.In typescript, which symbol is commonly used to translate a statement?",
+        choices: [".", ":", ";", ","]
+    },
+    {
+        name: "question_5",
+        type: "list",
+        message: "Q1.Which method of Inquirer.js is used to start the prompt interface and reeive user Input?",
+        choices: ["start()", "prompt()", "init()", "run()"]
+    }
+]);
+let score = 0;
+switch (quiz.question_1) {
+    case "a !== b":
+        console.log("1.Correct!");
+        ++score;
+        break;
+    default:
+        console.log("1. Incorrect!");
+}
+switch (quiz.question_2) {
+    case "$":
+        console.log("2.Correct!");
+        ++score;
+        break;
+    default:
+        console.log("2. Incorrect!");
+}
+switch (quiz.question_3) {
+    case "+":
+        console.log("3.Correct!");
+        ++score;
+        break;
+    default:
+        console.log("3. Incorrect!");
+}
+switch (quiz.question_4) {
+    case ";":
+        console.log("4.Correct!");
+        ++score;
+        break;
+    default:
+        console.log("4. Incorrect!");
+}
+switch (quiz.question_5) {
+    case "prompt()":
+        console.log("5.Correct!");
+        ++score;
+        break;
+    default:
+        console.log("5. Incorrect!");
+}
+console.log(`score: ${score}`);
